922130f1af87c3dad609b1eba46912120bbd11eb
Rezistan <taslas@hotmail.fr>
2019-04-16 17:49:21 +0200

    Filtering partially working

--- js/main.js
+++ js/main.js
@@ -1,24 +1,61 @@
 var listePays = [];
+var bd;
+$(document).ready(function(){
+    chargerDonnees('php/data.php');
+    createGrid();
+});
 
+/**
+ *
+ * @param lien
+ * @returns {never|*|{always, pipe, promise, state, catch, then}}
+ */
 function chargerDonnees(lien) {
     var data = $.Deferred();
     $.ajax({
-        type: 'GET',
+        type: "GET",
         url: lien,
-        dataType: 'json'
+        dataType: "json"
     }).done(function(response){
         listCountries(response);
-        bd.gens = response;
-        data.resolve(response);
+        buildController(response);
+        createGrid(response);
     });
     return data.promise();
 }
 
-$(document).ready(function(){
-    var donnees = chargerDonnees('php/data.php');
-    createGrid(donnees);
-});
+/**
+ *
+ */
+function buildController(datas){
+    bd = {
+        loadData: function(filter) {
+            return $.grep(this.gens, function(pers) {
+                return (!filter.nom || pers.nom.indexOf(filter.nom) > -1)
+                    && (!filter.naissance || pers.naissance.indexOf(filter.naissance) > -1)
+                    && (!filter.pays || pers.pays === filter.pays)
+                    && (filter.marie === undefined || pers.marie === filter.marie);
+            });
+        },
 
+        insertItem: function(insertingPers) {
+            this.gens.push(insertingPers);
+        },
+
+        updateItem: function(updatingPers) { },
+
+        deleteItem: function(deletingPers) {
+            var persIndex = $.inArray(deletingPers, this.gens);
+            this.gens.splice(persIndex, 1);
+        }
+    };
+    bd.gens = datas;
+    bd.countries = listePays;
+}
+
+/**
+ *
+ */
 function createGrid(){
     $('#gridPers').jsGrid({
         width: '100%',
@@ -44,27 +81,3 @@ function listCountries(data){
     });
     listePays.sort();
 }
-
-var bd = {
-    loadData: function(filter) {
-        return $.grep(this.gens, function(pers) {
-            return (!filter.nom || pers.nom.indexOf(filter.nom) > -1)
-                && (!filter.naissance || pers.naissance.indexOf(filter.naissance) > -1)
-                && (!filter.pays || pers.pays === filter.pays)
-                && (filter.marie === undefined || pers.marie === filter.marie);
-        });
-    },
-
-    insertItem: function(insertingPers) {
-        this.gens.push(insertingPers);
-    },
-
-    updateItem: function(updatingPers) { },
-
-    deleteItem: function(deletingPers) {
-        var persIndex = $.inArray(deletingPers, this.gens);
-        this.gens.splice(persIndex, 1);
-    }
-};
-
-bd.countries = listePays;
