3dc9c6207f40b168bd68298feb24a9820cbb7aee
Rezistan <taslas@hotmail.fr>
2019-04-24 17:09:34 +0200

    customizing sorting icons on progress 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var listePays = [];
+let listePays = [];
 
 /**
  *
@@ -6,14 +6,14 @@ var listePays = [];
  * @returns {{never || always, pipe, promise, state, catch, then}}
  */
 function chargerDonnees(lien) {
-    var data = $.Deferred();
+    let data = $.Deferred();
     $.ajax({
         type: "GET",
         url: lien,
         dataType: "json"
     }).done(function(response){
         listCountries(response);
-        var controller = buildController(response);
+        let controller = buildController(response);
         createGrid(controller);
     });
     return data.promise();
@@ -24,14 +24,14 @@ function chargerDonnees(lien) {
  * et de filtrage pour la recherche
  */
 function buildController(datas){
-    var bd = {
+    let bd = {
         loadData: function(filter) {
-            var timeFrom = new Date(filter.mariage.from).getTime();
-            var timeTo = new Date(filter.mariage.to).getTime();
+            let timeFrom = new Date(filter.mariage.from).getTime();
+            let timeTo = new Date(filter.mariage.to).getTime();
             return $.grep(this.gens, function(pers) {
                 //formatage de la date
-                var asDate = pers.mariage.split('/');
-                var dateN = new Date(asDate[2], asDate[1]-1, asDate[0]);
+                let asDate = pers.mariage.split('/');
+                let dateN = new Date(asDate[2], asDate[1]-1, asDate[0]);
                 return (!filter.nom || pers.nom.toUpperCase().indexOf(filter.nom.toUpperCase()) > -1)
                     && ((!filter.mariage.from && !filter.mariage.to) || dateRange(timeFrom, timeTo, dateN))
                     && (filter.pays.length === 0 || filter.pays.includes(pers.pays.toString()))
@@ -91,21 +91,21 @@ function createGrid(ctrl){
             });
         },
         headerRowRenderer: function() {
-            var $result = $("<tr>").append($("<th id='0' rowspan='2'>").text("Nom"));
+            let $result = $("<tr>").append($("<th id='0' rowspan='2'>").text("Nom"));
             $result.append($("<th id='1' rowspan='2'>").text("Pays"));
             $result.append($("<th colspan='2'>").text("Mariage"));
-            var secLine = $("<tr>").append($("<th id='2'>").text("Majorité"));
+            let secLine = $("<tr>").append($("<th id='2'>").text("Majorité"));
             secLine.append($("<th id='3'>").text("Date"));
             $result = $result.add(secLine);
 
-            var grid = this;
+            let grid = this;
             $(document).on('click', '.jsgrid-table tr th', function (e) {
-                var index = e.currentTarget.id;
+                let index = e.currentTarget.id;
                 if(grid.sorting && !isNaN(parseInt(index))){
                     grid.sort(index);
                     //customisation des icones de tri
-                    var elem = document.getElementById(index);
-                    if (elem.className.includes('desc')) {
+                    let elem = document.getElementById(index);
+                    if (elem.className.includes('-desc')) {
                         $('.asc').add($('.desc')).removeClass('asc').removeClass('desc');
                         elem.classList.add('desc');
                     } else {
@@ -123,7 +123,7 @@ function createGrid(ctrl){
  * champ custom du multiselect
  */
 function customMultiSelect(){
-    var multiselectField = function(config) {
+    let multiselectField = function(config) {
         jsGrid.Field.call(this, config);
     };
 
@@ -133,13 +133,13 @@ function customMultiSelect(){
         valueField: "",
 
         _createSelect: function(grid, selected) {
-            var textField = this.textField;
-            var valueField = this.valueField;
-            var $result = $("<select id='selectPays' multiple data-container='body' data-actions-box='true' data-size='12'>");
+            let textField = this.textField;
+            let valueField = this.valueField;
+            let $result = $("<select id='selectPays' multiple data-container='body' data-actions-box='true' data-size='12'>");
             $.each(this.items, function(_, item) {
-                var text = item[textField];
-                var val = item[valueField];
-                var $opt = $("<option>").val(val).text(text);
+                let text = item[textField];
+                let val = item[valueField];
+                let $opt = $("<option>").val(val).text(text);
                 if($.inArray(val, selected) > -1) {
                     $opt.attr("selected", "selected");
                 }
@@ -159,7 +159,7 @@ function customMultiSelect(){
         },
 
         itemTemplate: function(value) {
-            for(var i in listePays){
+            for(let i in listePays){
                 if(listePays[i].id === value){
                     return listePays[i].name;
                 }
@@ -171,7 +171,7 @@ function customMultiSelect(){
         },
 
         filterValue: function() {
-            var selected = [];
+            let selected = [];
             this._filterControl.find("option:selected").map(function() {
                 this.selected ? selected.push($(this).val()) : null;
             });
@@ -187,16 +187,16 @@ function customMultiSelect(){
  * champ custom de la date
  */
 function customDate(){
-    var dateField = function(config) {
+    let dateField = function(config) {
         jsGrid.Field.call(this, config);
     };
 
     dateField.prototype = new jsGrid.Field({
         sorter: function(date1, date2) {
-            var asDate1 = date1.split('/');
-            var asDate2 = date2.split('/');
-            var tDate1 = new Date(asDate1[2], asDate1[1]-1, asDate1[0]);
-            var tDate2 = new Date(asDate2[2], asDate2[1]-1, asDate2[0]);
+            let asDate1 = date1.split('/');
+            let asDate2 = date2.split('/');
+            let tDate1 = new Date(asDate1[2], asDate1[1]-1, asDate1[0]);
+            let tDate2 = new Date(asDate2[2], asDate2[1]-1, asDate2[0]);
             return tDate1 - tDate2;
         },
 
@@ -205,7 +205,7 @@ function customDate(){
         },
 
         filterTemplate: function() {
-            var grid = this._grid;
+            let grid = this._grid;
 
             this._dateDebut = $("<input>").width(103).datepicker({format: 'dd/mm/yyyy', horizontal: 'left', vertical: 'bottom'});
             this._dateFin = $("<input>").width(103).datepicker({format: 'dd/mm/yyyy'});
